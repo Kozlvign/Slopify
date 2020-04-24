b28eb1d7a53ea383ffe1e108bcd0fe129ead80c6
Kozlvign <63605496+Kozlvign@users.noreply.github.com>
2020-04-24 14:34:32 -0300

    changes

--- register.php
+++ register.php
@@ -12,7 +12,7 @@
         if(isset($_POST[$name])) {
             echo $_POST[$name];
         }
-    }
+    };
 ?>
 
 <html lang="en">
@@ -26,6 +26,7 @@
             <form action="register.php" id="loginForm" method="POST">
                 <h2>Login to your account</h2>
                 <p>
+                    <?php echo $account->getError(Constants::$loginFailed); ?>
                     <label for="loginUsername">Username</label>
                     <input id=loginUsername" name="loginUsername" type="text" placeholder=" Username" required>
                 </p>
@@ -40,11 +41,12 @@
                 <h2>Create your free account</h2>
                 <p>
                     <?php echo $account->getError(Constants::$usernameCharacters); ?>
+                    <?php echo $account->getError(Constants::$usernameTaken); ?>
                     <label for="username">Username</label>
                     <input id=username" name="username" type="text" placeholder="Username" value="<?php getInputValue('username') ?>" required>
                 </p>
                 <p>
-                <?php echo $account->getError(Constants::$firstNameCharacters); ?>
+                    <?php echo $account->getError(Constants::$firstNameCharacters); ?>
                     <label for="firstName">First Name</label>
                     <input id=firstName" name="firstName" type="text" placeholder="First Name" value="<?php getInputValue('firstName') ?>" required>
                 </p>
@@ -57,6 +59,7 @@
                 <p>
                     <?php echo $account->getError(Constants::$emailsDoNotMatch); ?>
                     <?php echo $account->getError(Constants::$emailInvalid); ?>
+                    <?php echo $account->getError(Constants::$emailTaken); ?>
                     <label for="email">Email</label>
                     <input id=email" name="email" type="email" placeholder="exemple@gmail.com" value="<?php getInputValue('email') ?>" required>
                 </p>
